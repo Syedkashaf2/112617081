fc65525a8bc0cfbbf4edc4e2322eff4dd7adb403
Kang <syeddkashaf111@gmail.com>
2026-09-06 20:46:54 +0530

    new q

---
+++ questions/6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vectors>
+using namespace std;
+
+int main()
+{
+	vector<int> frames {120, -50, 105, 110, -20, -5, 130, 95};
+	vector<int> clean_frames;
+	int count{};
+
+	for (int i{}; i < 8; i++)
+	{
+		if (frames.at(i) > 0)
+		{
+			clean_frames.push_back(frames.at(i));
+			count ++;
+		}	
+	}
+
+	cout << clean_frames.size() << '\n';
+	
+	for ( int i{}; i < count; i++)
+	{
+		cout << clean_frames.at(i) << "  ";
+	}
+
+	return 0;
+}
