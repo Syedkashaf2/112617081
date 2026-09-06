c1c39ff7fd19c66691eeed29b740e427e0377bb4
Kang <syeddkashaf111@gmail.com>
2026-09-06 20:05:59 +0530

    question

---
+++ questions/7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+	int n{10};
+	int votes[10] {2, 4, 2, 1, 2, 3, 4, 2, 1, 4};
+	int vote_counts[5] {};
+	int k {4};
+	int current{0};
+	int max{};
+
+	for ( int i{0}; i < 10; i++ )
+	{
+		vote_counts[votes[i]]++;
+	}
+
+	for ( int i{}; i < 5; i++)
+	{
+		current = votes[i];
+	       if (current > max )
+	       {
+		max = current;
+	       }	       
+	}
+	cout << max << '\n';
+	return 0;
+}
