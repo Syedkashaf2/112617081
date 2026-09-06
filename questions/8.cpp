e17c4d7ee650a1b744fb8c22a306732f56714d47
Kang <syeddkashaf111@gmail.com>
2026-09-06 19:04:59 +0530

    question 8 done

--- questions/8.cpp
+++ questions/8.cpp
@@ -1,14 +1,29 @@
-#include <iostraem>
+#include <iostream>
 //#include <vector>
 using namespace std;
 
 int main()
 {	
+	int current_streak{1}, max_streak{1}, n{7};
 	int a[] {12, 15, 22, 10, 18, 25, 30};
-	int current_streak{1}, max_streak{1};
-	for (int i{}; i < 
+	for (int i{1}; i < n; i++)
+	{
+		if ( a[i] >  a[i-1])
+		{
+			current_streak++;
+		} else 
+		{
+			current_streak = 1;
+			max_streak = 1;
+		}
 
+		if ( current_streak > max_streak )
+		{
+			max_streak = current_streak;
+		}
 
+	}	
+	cout << max_streak << '\n';
 
-	retrurn 0;
+	return 0;
 }
