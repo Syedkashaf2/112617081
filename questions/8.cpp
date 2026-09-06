11097942be145528f304eb05226ee20e8a3b04aa
kAng <syedkashaf111@gmail.com>
2026-09-06 18:28:19 +0530

    Urgent

---
+++ questions/8.cpp
@@ -0,0 +1,14 @@
+#include <iostraem>
+//#include <vector>
+using namespace std;
+
+int main()
+{	
+	int a[] {12, 15, 22, 10, 18, 25, 30};
+	int current_streak{1}, max_streak{1};
+	for (int i{}; i < 
+
+
+
+	retrurn 0;
+}
